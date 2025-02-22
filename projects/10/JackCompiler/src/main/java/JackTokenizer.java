758fd58b832749661874a192ad8c8454ba25cfa4
StarkiJ <132117607+StarkiJ@users.noreply.github.com>
2025-02-23 04:37:28 +0800

    P10: WIP (JackTokenizer)
    encountered difficulties in the method of reading the file

--- projects/10/JackCompiler/src/main/java/JackTokenizer.java
+++ projects/10/JackCompiler/src/main/java/JackTokenizer.java
@@ -1,11 +1,23 @@
+import java.util.HashSet;
 import java.util.Scanner;
 import java.io.*;
+import java.util.Set;
 
 // 从输入流中删除所有的注释和空格，
 // 并根据Jack语法的规则将输入流分解成Jack语言的字元（终结符）
 public class JackTokenizer {
     private Scanner scanner;
+    private String[] currentLine;
+    private int currentLineIndex;
     private String currentToken;
+    private String tokenType;
+    private static final Set<String> keywords = Set.of(
+            "class", "constructor", "function", "method", "field", "static", "var",
+            "int", "char", "boolean", "void", "true", "false", "null", "this",
+            "let", "do", "if", "else", "while", "return");
+    private static final Set<String> symbols = Set.of(
+            "{", "}", "(", ")", "[", "]", ".", ",", ";",
+            "+", "-", "*", "/", "&", "|", "<", ">", "=", "~");
 
     // 构造函数：打开输入文件/输入流，准备进行字符转换操作
     public JackTokenizer(String input) {
@@ -18,8 +30,14 @@ public class JackTokenizer {
 
     // 输入中是否还有字元？
     public boolean hasMoreTokens() {
-        if (scanner.hasNext()) {
+        if (currentLineIndex < currentLine.length) {
             return true;
+        } else if (scanner.hasNextLine()) {
+            currentLine = scanner.nextLine()
+                    .replaceAll("//.*|/\\*|\\*|/\\*/", "")
+                    .trim().split("\\s+");
+            currentLineIndex = 0;
+            return hasMoreTokens();
         } else {
             scanner.close();
             return false;
@@ -30,12 +48,32 @@ public class JackTokenizer {
     // 该函数仅当hasMoreTokens()返回为真时才能调用。
     // 最初始状态是没有当前字元
     public void advance() {
-
+        currentToken = currentLine[currentLineIndex++];
     }
 
     // 返回当前字元的类型
     public String tokenType() {
-        return null;
+//        return switch (currentToken) {
+//            case "class", "constructor", "function", "method", "field", "static", "var",
+//                 "int", "char", "boolean", "void", "true", "false", "null", "this",
+//                 "let", "do", "if", "else", "while", "return" -> "KEYWORD";
+//            case "{", "}", "(", ")", "[", "]", ".", ",", ";",
+//                 "+", "-", "*", "/", "&", "|", "<", ">", "=", "~" -> "SYMBOL";
+//            case "\"" -> "STRING_CONST";
+//            case "[0-9]+" -> "INT_CONST";
+//            default -> "IDENTIFIER";
+//        };
+        if (keywords.contains(currentToken)) {
+            return "KEYWORD";
+        } else if (symbols.contains(currentToken)) {
+            return "SYMBOL";
+        } else if (currentToken.startsWith("\"")) {
+            return "STRING_CONST";
+        } else if (currentToken.matches("[0-9]+")) {
+            return "INT_CONST";
+        } else {
+            return "IDENTIFIER";
+        }
     }
 
     // 返回当前字元的关键字。
